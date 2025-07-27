23ab2670599a2d30ee2c2b9ad1c1ab5f3df5cf88
lapiedranumerodos <maria.orgniani@estudiante.imagecampus.edu.ar>
2025-07-27 19:35:29 -0300

    add module 6 collisions

--- scripts/scenes/2D_level.gd
+++ scripts/scenes/2D_level.gd
@@ -1,14 +1,15 @@
 extends Node2D
 
 @export var modules: Array[PackedScene] = []
-
 @export var background: ParallaxBackground
-@export var scroll_speed := Vector2(-50, 0) # pixels per second, X scroll only
+@export var scroll_speed := Vector2(-50, 0)
 
 var rng = RandomNumberGenerator.new()
-var amnt = 30
-var offset = 160
-var initObs = 0
+var amnt := 30
+var offset := 160
+var initObs := 0
+
+var _last_spawn_x := -INF
 
 func _ready():
 	for n in amnt:
@@ -18,14 +19,20 @@ func _process(delta):
 	background.scroll_offset += scroll_speed * delta
 
 func spawn_module(x_position: float):
-	if initObs > 5:
-		rng.randomize()
-		var num = rng.randi_range(0, modules.size() - 1)
-		var instance = modules[num].instantiate()
-		instance.position.x = x_position
-		add_child(instance)
-	else:
+	if abs(x_position - _last_spawn_x) < offset * 0.5:
+		return
+
+	_last_spawn_x = x_position
+
+	if initObs <= 5:
 		var instance = modules[0].instantiate()
-		instance.position.x = x_position
+		instance.position = Vector2(x_position, 0)
 		add_child(instance)
 		initObs += 1
+		return
+
+	rng.randomize()
+	var num = rng.randi_range(0, modules.size() - 1)
+	var instance = modules[num].instantiate()
+	instance.position = Vector2(x_position, 0)
+	add_child(instance)
